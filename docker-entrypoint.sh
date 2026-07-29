955808063149a5240403ffdf431a0d830881aac9
Plamen Petkov <plamen.petkov@pytek.bg>
2026-07-29 12:26:17 +0300

    adding security bundle

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,9 +14,8 @@ php bin/console cache:clear --env=prod
 
 echo "Starting Apache..."
 
-exec apache2-foreground
 
 sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf
 sed -i "s/80/${PORT}/g" /etc/apache2/ports.conf
 
-chmod +x docker-entrypoint.sh
+exec apache2-foreground
